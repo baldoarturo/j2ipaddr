4bab331217a3df2c79b808808e702f7374aa73a5
Arturo Baldo <baldoarturo@gmail.com>
2022-10-01 18:33:35 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
   
 setuptools.setup(
     name="j2ipaddr",
-    version="0.0.2",
+    version="0.1.0",
     author="Arturo Baldo",
     author_email="baldoarturo@gmail.com",
     packages=["src"],
