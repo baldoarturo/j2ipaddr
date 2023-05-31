87ed9a53cbdfa9f8d664bc47bf15f08399bda7dc
Arturo <arturo.baldo@iparchitechs.com>
2023-05-31 12:09:10 -0300

    Fix tests

--- test_functions.py
+++ test_functions.py
@@ -1,7 +1,7 @@
 import pytest
 from j2ipaddr import filters
 
-test_address = '198.51.100.1/24'
+test_address = '198.51.100.4/29'
 
 
 def test_load_all():
@@ -9,40 +9,40 @@ def test_load_all():
 
 
 def test_ip_address():
-    assert filters.ip_address(test_address)
+    assert filters.ip_address(test_address), '198.51.100.4'
 
 
 def test_ip_prefixlen():
-    assert filters.ip_prefixlen(test_address)
+    assert filters.ip_prefixlen(test_address), '29'
 
 
 def test_ip_netmask():
-    assert filters.ip_netmask(test_address)
+    assert filters.ip_netmask(test_address), '255.255.255.248'
 
 
 def test_ip_hostmask():
-    assert filters.ip_hostmask(test_address)
+    assert filters.ip_hostmask(test_address), '0.0.0.7'
 
 
 def test_ip_wildcard():
-    assert filters.ip_wildcard(test_address)
+    assert filters.ip_wildcard(test_address), '0.0.0.7'
 
 
 def test_ip_network():
-    assert filters.ip_network(test_address)
+    assert filters.ip_network(test_address), '198.51.100.0'
 
 
 def test_ip_broadcast():
-    assert filters.ip_broadcast(test_address)
+    assert filters.ip_broadcast(test_address), '198.51.100.7'
 
 
 def test_ip_network_hosts_size():
-    assert filters.ip_network_hosts_size(test_address)
+    assert filters.ip_network_hosts_size(test_address), 6
 
 
 def test_ip_network_first():
-    assert filters.ip_network_first(test_address)
+    assert filters.ip_network_first(test_address), '198.51.100.1'
 
 
 def test_ip_network_last():
-    assert filters.ip_network_last(test_address)
+    assert filters.ip_network_last(test_address), '198.51.100.6'
